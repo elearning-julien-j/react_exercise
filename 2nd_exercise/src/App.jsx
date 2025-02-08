81fd407d64c7499e1407115450627c9b30254718
padcmoi <juliennaskot@gmail.com>
2025-02-09 00:07:43 +0100

    feat(2nd-exercise): add counter with increment and decrement counter

---
+++ 2nd_exercise/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => count && setCount(count - 1);
+
+  return (
+    <>
+      <div>Compteur: {count}</div>
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
