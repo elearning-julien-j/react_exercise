bcbb0e8f180d9c0783d6b0e51656b0ded3dc35b0
padcmoi <juliennaskot@gmail.com>
2025-02-09 00:54:24 +0100

    feat: add 2nd exercise step 3

--- 2nd_exercise/src/App.jsx
+++ 2nd_exercise/src/App.jsx
@@ -2,32 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState("");
-
-  const removeTask = (index) => setTasks(tasks.filter((_, idx) => idx !== index));
-
-  function addTask() {
-    if (task.trim() !== "") {
-      setTasks([...tasks, { id: crypto.randomUUID(), task }]);
-      setTask("");
-    }
-  }
+  const [isVisible, setVisible] = useState(false);
 
   return (
     <>
-      <ul>
-        {tasks.map((obj, index) => (
-          <li key={obj.id}>
-            {obj.task} <button onClick={() => removeTask(index)}>Supprimer</button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => setVisible(!isVisible)}>Afficher / Cacher</button>
 
-      <div>
-        <input type="text" placeholder="nouvelle tâche" value={task} onChange={(e) => setTask(e.target.value)} />
-        <button onClick={addTask}>Ajouter</button>
-      </div>
+      {isVisible && <p>Contenu visible</p>}
     </>
   );
 }
