512acf940213beeb270b069211344ba2478c5457
padcmoi <juliennaskot@gmail.com>
2025-02-11 21:15:42 +0100

    init 6 step 3

--- 6th_exercise/src/Ex6Step3.jsx
+++ 6th_exercise/src/Ex6Step3.jsx
@@ -1,3 +1,33 @@
+import { useForm } from "react-hook-form";
+
 export default function App() {
-  return <div></div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Données soumises :", data);
+  };
+
+  return (
+    <div>
+      <h1>Inscription</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Nom :</label>
+          <input {...register("name")} />
+        </div>
+
+        <div>
+          <label>Email :</label>
+          <input {...register("email")} />
+        </div>
+
+        <div>
+          <label>Mot de passe :</label>
+          <input type="password" {...register("password")} />
+        </div>
+
+        <button type="submit">S'inscrire</button>
+      </form>
+    </div>
+  );
 }
