aba253c44a9839a22a34dacdc09ff39497c907c3
padcmoi <juliennaskot@gmail.com>
2025-02-11 14:59:06 +0100

    install dependencies yup & react-hook-form / base code

--- 6th_exercise/src/App.jsx
+++ 6th_exercise/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Données soumises :", data);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Inscription</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Nom :</label>
+          <input {...register("name")} />
+        </div>
 
-export default App
+        <div>
+          <label>Email :</label>
+          <input {...register("email")} />
+        </div>
+
+        <div>
+          <label>Mot de passe :</label>
+          <input type="password" {...register("password")} />
+        </div>
+
+        <button type="submit">S'inscrire</button>
+      </form>
+    </div>
+  );
+}
