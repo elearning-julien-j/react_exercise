8c1cc296acaea17f4bf38ebf2b47650230b5778b
padcmoi <juliennaskot@gmail.com>
2025-02-11 21:14:58 +0100

    init 6 step 3

--- 6th_exercise/src/main.jsx
+++ 6th_exercise/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import Ex6Step2 from "./Ex6Step2";
+import Ex6Step3 from "./Ex6Step3";
 
-createRoot(document.getElementById("root")).render(<Ex6Step2></Ex6Step2>);
+createRoot(document.getElementById("root")).render(<Ex6Step3></Ex6Step3>);
