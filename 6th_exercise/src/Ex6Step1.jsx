ba9f4cf16a00fadcf5ad71180d519205e71354d9
padcmoi <juliennaskot@gmail.com>
2025-02-11 15:13:56 +0100

    exercise 6 step 1 ok

--- 6th_exercise/src/Ex6Step1.jsx
+++ 6th_exercise/src/Ex6Step1.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export default function App() {
+  const schema = yup.object().shape({
+    name: yup.string().min(3, "Minimum 3 caractères (pour le nom)").required("Le nom est requis"),
+    email: yup.string().email("Email invalide").required("L'email est requis"),
+    password: yup.string().min(6, "Minimum 6 caractères (pour le mot de passe)").required("Le mot de passe est requis"),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: yupResolver(schema) });
 
   const onSubmit = (data) => {
     console.log("Données soumises :", data);
+    alert("Inscription réussie");
   };
 
   return (
@@ -19,16 +28,19 @@ export default function App() {
         <div>
           <label>Nom :</label>
           <input {...register("name")} />
+          {errors.name && <p>{errors.name.message}</p>}
         </div>
 
         <div>
           <label>Email :</label>
           <input {...register("email")} />
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
 
         <div>
           <label>Mot de passe :</label>
           <input type="password" {...register("password")} />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
 
         <button type="submit">S'inscrire</button>
--- 6th_exercise/src/main.jsx
+++ 6th_exercise/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Ex6Step1 from "./Ex6Step1";
+
+createRoot(document.getElementById("root")).render(<Ex6Step1></Ex6Step1>);
